f0596771023964e2147218aaaf0c0d6bcdaea029
me-coderaider <kashyappankajnsp@gmail.com>
2024-09-30 00:18:10 +0530

    added insert new user route

--- social-repo/src/routes/users.js
+++ social-repo/src/routes/users.js
@@ -1,31 +1,36 @@
-const express=require('express');
-const userRepo = require('../repos/user-repo');
-const router=express.Router();
+const express = require("express");
+const userRepo = require("../repos/user-repo");
+const router = express.Router();
 
-router.get("/users",async (req,res)=>{
-    // for each route we've to perform 2 things
-    // 1. Run a query as per the route.
-    // 2.Send the result back to the person who made the request.
-    const users=await userRepo.find();
-    res.send(users);
+router.get("/users", async (req, res) => {
+  // for each route we've to perform 2 things
+  // 1. Run a query as per the route.
+  // 2.Send the result back to the person who made the request.
+  const users = await userRepo.find();
+  res.send(users);
 });
 
-router.get("/users/:id",async(req,res)=>{
-    const {id}=req.params;
+router.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
 
-    const user = await userRepo.findById(id);
+  const user = await userRepo.findById(id);
 
-    if(user){
-        res.send(user);
-    }else{
-        res.sendStatus(404);
-    }
+  if (user) {
+    res.send(user);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
-router.post("/users",async(req,res)=>{});
+router.post("/users", async (req, res) => {
+  const { username, bio } = req.body;
 
-router.put("/users/:id", async(req,res)=>{});
+  const user = await userRepo.insert(username, bio);
+  res.send(user);
+});
 
-router.delete("/users/:id",async(req,res)=>{});
+router.put("/users/:id", async (req, res) => {});
 
-module.exports=router;
+router.delete("/users/:id", async (req, res) => {});
+
+module.exports = router;
