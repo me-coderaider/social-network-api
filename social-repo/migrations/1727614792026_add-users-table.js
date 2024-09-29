c9b49b27e714fbdedbc5e3d847e04e8b7cfd92d0
me-coderaider <kashyappankajnsp@gmail.com>
2024-09-29 18:43:35 +0530

    taking file again

--- social-repo/migrations/1727614792026_add-users-table.js
+++ social-repo/migrations/1727614792026_add-users-table.js
@@ -8,11 +8,25 @@ exports.shorthands = undefined;
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-exports.up = (pgm) => {};
+exports.up = (pgm) => {
+    pgm.sql(`
+        CREATE TABLE users(
+            id SERIAL PRIMARY KEY,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            bio VARCHAR(400),
+            username VARCHAR(30) NOT NULL
+        );
+    `);
+};
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-exports.down = (pgm) => {};
+exports.down = (pgm) => {
+    pgm.sql(`
+        DROP TABLE users;
+    `);
+};
